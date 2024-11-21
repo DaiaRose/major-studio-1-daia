de04c3ad7d61a78be4dfdf4e7c674a34d122802e
Daia Bromberg <bromd514@newschool.edu>
2024-11-21 10:56:52 -0500

    data cleaned and vis start

--- Project03/02-interactivity copy/app.js
+++ Project03/02-interactivity copy/app.js
@@ -1,27 +1,12 @@
+const TOOLTIP_WIDTH = 150;
+const TOOLTIP_HEIGHT = 20;
 
-// using constants makes it easy to update when values change
-// many editors also autocomplete variable names
-// by convention, constants in JS have UPPERCASE names
-
-const CLASS = "class";
-const PETAL_LENGTH = "petallength";
-const PETAL_WIDTH =  "petalwidth";
-const SEPAL_LENGTH = "sepallength";
-const SEPAL_WIDTH = "sepalwidth";
-const TOOLTIP_WIDTH =  150;
-const TOOLTIP_HEIGHT =  20;
-
-
-// we can set up our state schema before we have any data
+// Initial state
 let state = {
   data: [],
-  filters: {
-    menu: [],
-    checked: [],
-  },
-  sizeBy: {
-    menu: [PETAL_LENGTH, PETAL_WIDTH, SEPAL_LENGTH, SEPAL_WIDTH],
-    selected: PETAL_LENGTH,
+  groupBy: {
+    menu: ["type", "material", "county"],
+    selected: "type", // Default grouping
   },
   tooltip: {
     value: "",
@@ -31,66 +16,55 @@ let state = {
   dimensions: [window.innerWidth, window.innerHeight],
 };
 
-// initializing these globally will be useful later
+// Initialize scales globally
 let xScale, yScale, colorScale;
 
+// Load and process data
 async function dataLoad() {
-  // we can set up our layout before we have data
   initializeLayout();
-  const data = await d3.json("./iris_json.json");
+  const data = await d3.json("cleaned_data.json");
 
-  // once data is on state, we can access it from any other function because state is a global variable
-  
-  // we also populate our checkboxes with values from the data
-  const checkboxValues = Array.from(new Set(data.map(d => d[CLASS])));
+  // Filter out entries with missing years
+  const validData = data.filter(d => d.year !== null);
 
-  // copy the data into the state variable, add a unique ID for each object and add the filters
   setState({
-    data: data.map((d, i) => ({
+    data: validData.map((d, i) => ({
       ...d,
-      id: d[CLASS] + "_" + i, // each object should have a unique ID
+      id: d.ID || `item_${i}`, // Ensure unique IDs
     })),
-    filters: {
-      menu: checkboxValues,
-      checked: checkboxValues,
-    },
   });
 }
 
-// whenever state changes, update the state variable, then redraw the viz
+// Update state and redraw the visualization
 function setState(nextState) {
-  // console.log("state updated");
-  // using Object.assign keeps the state *immutable*
-  state = Object.assign({}, state, nextState);//can have many sources for one target
+  state = Object.assign({}, state, nextState); // Immutable state update
   draw();
 }
 
-function onRadioChange(d) {
-  console.log(d)
-  const nextSelected = d.target.value;
+// Handle category toggle
+function onGroupByChange(d) {
   setState({
-    sizeBy: {
-      ...state.sizeBy,
-      selected: nextSelected,
+    groupBy: {
+      ...state.groupBy,
+      selected: d.target.value,
     },
   });
 }
 
+// Handle mouse events for tooltip
 function onMouseEvent(d) {
   if (d.type === "mouseenter") {
-    console.log("mouseenter")
     setState({
       tooltip: {
-        value: d.target.__data__,
+        value: d.target.__data__[state.groupBy.selected],
         visible: true,
         coordinates: [
-          +d3.select(d.target).attr("width") + TOOLTIP_WIDTH / 2 + 10,
-          +d3.select(d.target).attr("y") - TOOLTIP_HEIGHT / 2,
+          +d3.select(d.target).attr("cx"),
+          +d3.select(d.target).attr("cy") - 10,
         ],
       },
     });
   } else if (d.type === "mouseleave") {
-    console.log("mouseleave")
     setState({
       tooltip: {
         ...state.tooltip,
@@ -101,9 +75,9 @@ function onMouseEvent(d) {
   }
 }
 
-// this function sets up everything we can before data loads
+// Initialize layout
 function initializeLayout() {
-  const svgWidth = 0.6 * state.dimensions[0];
+  const svgWidth = 4 * state.dimensions[0]; // Increase width for better spacing
   const svgHeight = state.dimensions[1];
   const margin = 80;
 
@@ -113,434 +87,86 @@ function initializeLayout() {
     .attr("width", svgWidth)
     .attr("height", svgHeight);
 
-  xScale = d3.scalePoint()
-    .range([margin, svgWidth - margin])
-    .padding(0.5); // For categorical data on the x-axis
+  xScale = d3.scalePoint().range([margin, svgWidth - margin]).padding(0.5);
+  yScale = d3.scaleLinear().range([svgHeight - margin, margin]);
+  colorScale = d3.scaleOrdinal(d3.schemeDark2);
 
-  yScale = d3.scaleLinear()
-    .range([svgHeight - margin, margin]); // Flips the y-axis for stacking up
-
-  colorScale = d3.scaleOrdinal(d3.schemeDark2); // For categorical coloring
-
-  // Add x axis
-  svg
-    .append("g")
-    .attr("class", "axis x-axis")
-    .attr("transform", `translate(0, ${svgHeight - margin})`);
-
-  // Add y axis
-  svg
-    .append("g")
-    .attr("class", "axis y-axis")
-    .attr("transform", `translate(${margin}, 0)`);
+  // Add axes
+  svg.append("g").attr("class", "axis x-axis").attr("transform", `translate(0, ${svgHeight - margin})`);
+  svg.append("g").attr("class", "axis y-axis").attr("transform", `translate(${margin}, 0)`);
 
   svg.append("g").attr("class", "dots");
 
-  const tooltip = svg.append("g").attr("class", "tooltip");
-  tooltip
-    .append("rect")
-    .attr("height", TOOLTIP_HEIGHT)
-    .attr("width", TOOLTIP_WIDTH)
-    .attr("fill", "#fff")
-    .attr("stroke", "#000");
-  tooltip
-    .append("text")
-    .attr("x", 5)
-    .attr("y", 14)
-    .attr("font-size", 13);
-
-  // Add left menu
-  const leftMenu = parent.append("div").attr("class", "left-menu");
-  leftMenu.append("div").attr("class", "title").html(`
-      <h1>Fisher's Iris Dataset</h1>
-      <h4>Which properties of a flower best distinguish it from other species?</h4>
-    `);
-  leftMenu.append("div").attr("class", "filters");
-
-  // Add right menu
+  // Add menu to toggle grouping
   const rightMenu = parent.append("div").attr("class", "right-menu");
   rightMenu
     .append("form")
     .html(
-      state.sizeBy.menu
+      state.groupBy.menu
         .map(
           d =>
-            `<input type="radio" name="sizeby" value="${d}" ${
-              state.sizeBy.selected === d ? "checked" : ""
+            `<input type="radio" name="groupby" value="${d}" ${
+              state.groupBy.selected === d ? "checked" : ""
             }>${d}<br>`
         )
         .join("")
     )
-    .on("change", onRadioChange);
-  rightMenu.append("div").attr("class", "legend");
+    .on("change", onGroupByChange);
 }
 
-
-// everything in this function depends on data, so the function is called after data loads and whenever state changes
+// Draw the visualization
 function draw() {
-  const filteredData = state.data;
+  // Group data by year
+  const groupedData = d3.group(state.data, d => d.year);
+  const years = Array.from(groupedData.keys()).sort((a, b) => a - b);
 
-  console.log("filteredData:", filteredData);
+  xScale.domain(years);
+  yScale.domain([0, d3.max(groupedData.values(), v => v.length) || 0]);
 
-  xScale.domain(state.sizeBy.menu);
-  yScale.domain([0, d3.max(filteredData, d => d[state.sizeBy.selected]) || 0]);
-  colorScale.domain([...new Set(filteredData.map(d => d[CLASS]))]);
+  // Update axes
+  d3.select(".x-axis").call(d3.axisBottom(xScale)).attr("transform", `translate(0, ${yScale.range()[0]})`);
+  d3.select(".y-axis").call(d3.axisLeft(yScale)).attr("transform", `translate(80, 0)`);
 
-  d3.select(".x-axis")
-    .call(d3.axisBottom(xScale))
-    .attr("transform", `translate(0, ${yScale.range()[0]})`);
-
-  d3.select(".y-axis")
-    .call(d3.axisLeft(yScale))
-    .attr("transform", `translate(80, 0)`); // Adjust for margin
-
+  // Stacking logic for y position
+  const yearCounts = new Map();
   const dots = d3
     .select(".dots")
     .selectAll("circle")
-    .data(filteredData, d => d.id)
+    .data(state.data, d => d.id)
     .join("circle")
-    .attr("cx", d => xScale(state.sizeBy.selected))
-    .attr("cy", d => yScale(d[state.sizeBy.selected]))
+    .attr("cx", d => xScale(d.year))
+    .attr("cy", d => {
+      const year = d.year;
+      if (!yearCounts.has(year)) yearCounts.set(year, 0);
+      const count = yearCounts.get(year);
+      yearCounts.set(year, count + 1);
+      return yScale(count); // Stack dots
+    })
     .attr("r", 5)
-    .attr("fill", d => colorScale(d[CLASS]))
+    .attr("fill", d => colorScale(d[state.groupBy.selected]))
     .on("mouseenter", onMouseEvent)
     .on("mouseleave", onMouseEvent);
 
-  dots.transition()
-    .duration(1000)
-    .attr("cx", d => xScale(state.sizeBy.selected))
-    .attr("cy", d => yScale(d[state.sizeBy.selected]))
-    .attr("r", 5)
-    .attr("fill", d => colorScale(d[CLASS]));
-
-  // Tooltip logic
-  const tooltip = d3.select(".tooltip");
-  tooltip
-    .attr(
-      "transform",
-      `translate(${state.tooltip.coordinates[0]}, ${
-        state.tooltip.coordinates[1]
-      })`
-    )
-    .classed("visible", state.tooltip.visible);
-  tooltip.select("text").text(() => {
-    const d = state.tooltip.value;
-    return `${d.id}: ${d[state.sizeBy.selected]}`;
-  });
-}
-
-
-  // update legend based on filteredData
+  // Update legend
+  const legendData = Array.from(new Set(state.data.map(d => d[state.groupBy.selected])));
   const legend = d3.select(".legend");
   legend
     .selectAll(".legend-row")
-    .data(state.filters.checked)
+    .data(legendData)
     .join("div")
     .attr("class", "legend-row")
     .html(
       d => `
       <div class="box" style="background-color:${colorScale(d)};"></div>
-      <div class="legend-label">${d}</div>
+      <div class="legend-label">${d || "Unknown"}</div>
     `
     );
+}
 
-// this function is only called once
+// Load data and start visualization
 dataLoad();
 
 
 
 
 
-
-
-///////////////////////////////////////
-
-// // using constants makes it easy to update when values change
-// // many editors also autocomplete variable names
-// // by convention, constants in JS have UPPERCASE names
-
-// const CLASS = "class";
-// const PETAL_LENGTH = "petallength";
-// const PETAL_WIDTH =  "petalwidth";
-// const SEPAL_LENGTH = "sepallength";
-// const SEPAL_WIDTH = "sepalwidth";
-// const TOOLTIP_WIDTH =  150;
-// const TOOLTIP_HEIGHT =  20;
-
-
-// // we can set up our state schema before we have any data
-// let state = {
-//   data: [],
-//   filters: {
-//     menu: [],
-//     checked: [],
-//   },
-//   sizeBy: {
-//     menu: [PETAL_LENGTH, PETAL_WIDTH, SEPAL_LENGTH, SEPAL_WIDTH],
-//     selected: PETAL_LENGTH,
-//   },
-//   tooltip: {
-//     value: "",
-//     visible: false,
-//     coordinates: [0, 0],
-//   },
-//   dimensions: [window.innerWidth, window.innerHeight],
-// };
-
-// // initializing these globally will be useful later
-// let xScale, yScale, colorScale;
-
-// async function dataLoad() {
-//   // we can set up our layout before we have data
-//   initializeLayout();
-//   const data = await d3.json("./iris_json.json");
-
-//   // once data is on state, we can access it from any other function because state is a global variable
-  
-//   // we also populate our checkboxes with values from the data
-//   const checkboxValues = Array.from(new Set(data.map(d => d[CLASS])));
-
-//   // copy the data into the state variable, add a unique ID for each object and add the filters
-//   setState({
-//     data: data.map((d, i) => ({
-//       ...d,
-//       id: d[CLASS] + "_" + i, // each object should have a unique ID
-//     })),
-//     filters: {
-//       menu: checkboxValues,
-//       checked: checkboxValues,
-//     },
-//   });
-// }
-
-// // whenever state changes, update the state variable, then redraw the viz
-// function setState(nextState) {
-//   // console.log("state updated");
-//   // using Object.assign keeps the state *immutable*
-//   state = Object.assign({}, state, nextState);//can have many sources for one target
-//   draw();
-// }
-
-// function onCheckboxChange(d) {
-//   console.log(d.target.name)
-//   // first, was the clicked box already checked or not?
-//   const index = state.filters.checked.indexOf(d.target.name);
-//   const isBoxChecked = index > -1;
-//   let nextCheckedValues;
-//   // if box is checked, uncheck it
-//   if (isBoxChecked) {
-//     nextCheckedValues = [
-//       ...state.filters.checked.slice(0, index),
-//       ...state.filters.checked.slice(index + 1),
-//     ];
-//     // otherwise, add it to the checked values
-//   } else {
-//     nextCheckedValues = [...state.filters.checked, d.target.name];
-//   }
-//   setState({
-//     filters: {
-//       ...state.filters,
-//       checked: nextCheckedValues,
-//     },
-//   });
-// }
-
-// function onRadioChange(d) {
-//   console.log(d)
-//   const nextSelected = d.target.value;
-//   setState({
-//     sizeBy: {
-//       ...state.sizeBy,
-//       selected: nextSelected,
-//     },
-//   });
-// }
-
-// function onMouseEvent(d) {
-//   if (d.type === "mouseenter") {
-//     console.log("mouseenter")
-//     setState({
-//       tooltip: {
-//         value: d.target.__data__,
-//         visible: true,
-//         coordinates: [
-//           +d3.select(d.target).attr("width") + TOOLTIP_WIDTH / 2 + 10,
-//           +d3.select(d.target).attr("y") - TOOLTIP_HEIGHT / 2,
-//         ],
-//       },
-//     });
-//   } else if (d.type === "mouseleave") {
-//     console.log("mouseleave")
-//     setState({
-//       tooltip: {
-//         ...state.tooltip,
-//         value: "",
-//         visible: false,
-//       },
-//     });
-//   }
-// }
-
-// // this function sets up everything we can before data loads
-// function initializeLayout() {
-//   const svgWidth = 0.6 * state.dimensions[0];
-//   const svgHeight = state.dimensions[1];
-//   const margin = 80;
-
-//   const parent = d3.select(".interactive");
-//   const svg = parent
-//     .append("svg")
-//     .attr("width", svgWidth)
-//     .attr("height", svgHeight);
-
-//   xScale = d3.scalePoint()
-//     .range([margin, svgWidth - margin])
-//     .padding(0.5); // For categorical data on the x-axis
-
-//   yScale = d3.scaleLinear()
-//     .range([svgHeight - margin, margin]); // Flips the y-axis for stacking up
-
-//   colorScale = d3.scaleOrdinal(d3.schemeDark2); // For categorical coloring
-
-//   // Add x axis
-//   svg
-//     .append("g")
-//     .attr("class", "axis x-axis")
-//     .attr("transform", `translate(0, ${svgHeight - margin})`);
-
-//   // Add y axis
-//   svg
-//     .append("g")
-//     .attr("class", "axis y-axis")
-//     .attr("transform", `translate(${margin}, 0)`);
-
-//   svg.append("g").attr("class", "dots");
-
-//   const tooltip = svg.append("g").attr("class", "tooltip");
-//   tooltip
-//     .append("rect")
-//     .attr("height", TOOLTIP_HEIGHT)
-//     .attr("width", TOOLTIP_WIDTH)
-//     .attr("fill", "#fff")
-//     .attr("stroke", "#000");
-//   tooltip
-//     .append("text")
-//     .attr("x", 5)
-//     .attr("y", 14)
-//     .attr("font-size", 13);
-
-//   // Add left menu
-//   const leftMenu = parent.append("div").attr("class", "left-menu");
-//   leftMenu.append("div").attr("class", "title").html(`
-//       <h1>Fisher's Iris Dataset</h1>
-//       <h4>Which properties of a flower best distinguish it from other species?</h4>
-//     `);
-//   leftMenu.append("div").attr("class", "filters");
-
-//   // Add right menu
-//   const rightMenu = parent.append("div").attr("class", "right-menu");
-//   rightMenu
-//     .append("form")
-//     .html(
-//       state.sizeBy.menu
-//         .map(
-//           d =>
-//             `<input type="radio" name="sizeby" value="${d}" ${
-//               state.sizeBy.selected === d ? "checked" : ""
-//             }>${d}<br>`
-//         )
-//         .join("")
-//     )
-//     .on("change", onRadioChange);
-//   rightMenu.append("div").attr("class", "legend");
-// }
-
-
-// // everything in this function depends on data, so the function is called after data loads and whenever state changes
-// function draw() {
-//   const filteredData = state.data
-//     .filter(d => state.filters.checked.indexOf(d[CLASS]) > -1);
-
-//   console.log("filteredData:", filteredData);
-
-//   xScale.domain(state.sizeBy.menu);
-//   yScale.domain([0, d3.max(filteredData, d => d[state.sizeBy.selected]) || 0]);
-//   colorScale.domain(state.filters.menu);
-
-//   d3.select(".x-axis")
-//     .call(d3.axisBottom(xScale))
-//     .attr("transform", `translate(0, ${yScale.range()[0]})`);
-
-//   d3.select(".y-axis")
-//     .call(d3.axisLeft(yScale))
-//     .attr("transform", `translate(${xScale.range()[0]}, 0)`);
-
-//   const dots = d3
-//     .select(".dots")
-//     .selectAll("circle")
-//     .data(filteredData, d => d.id)
-//     .join("circle")
-//     .attr("cx", d => xScale(state.sizeBy.selected))
-//     .attr("cy", d => yScale(d[state.sizeBy.selected]))
-//     .attr("r", 5)
-//     .attr("fill", d => colorScale(d[CLASS]))
-//     .on("mouseenter", onMouseEvent)
-//     .on("mouseleave", onMouseEvent);
-
-//   dots.transition()
-//     .duration(1000)
-//     .attr("cx", d => xScale(state.sizeBy.selected))
-//     .attr("cy", d => yScale(d[state.sizeBy.selected]))
-//     .attr("r", 5)
-//     .attr("fill", d => colorScale(d[CLASS]));
-
-
-//   // Tooltip logic
-//   const tooltip = d3.select(".tooltip");
-//   tooltip
-//     .attr(
-//       "transform",
-//       `translate(${state.tooltip.coordinates[0]}, ${
-//         state.tooltip.coordinates[1]
-//       })`
-//     )
-//     .classed("visible", state.tooltip.visible);
-//   tooltip.select("text").text(() => {
-//     const d = state.tooltip.value;
-//     return `${d.id}: ${d[state.sizeBy.selected]}`;
-//   });
-
-// }
-
-  
-//   // update tooltip based on state.tooltip
-//   const tooltip = d3.select(".tooltip");
-//   tooltip
-//     .attr(
-//       "transform",
-//       `translate(${state.tooltip.coordinates[0]}, ${
-//         state.tooltip.coordinates[1]
-//       })`
-//     )
-//     .classed("visible", state.tooltip.visible);
-//   tooltip.select("text").text(() => {
-//     const d = state.tooltip.value;
-//     return `${d.id}: ${d[state.sizeBy.selected]}`;
-//   });
-
-//   // update legend based on filteredData
-//   const legend = d3.select(".legend");
-//   legend
-//     .selectAll(".legend-row")
-//     .data(state.filters.checked)
-//     .join("div")
-//     .attr("class", "legend-row")
-//     .html(
-//       d => `
-//       <div class="box" style="background-color:${colorScale(d)};"></div>
-//       <div class="legend-label">${d}</div>
-//     `
-//     );
-
-// // this function is only called once
-// dataLoad();
---
+++ Project03/data/helperfilterDS.py
@@ -0,0 +1,189 @@
+import json
+import pandas as pd
+import re
+
+# Mappings
+type_mapping = {
+    'dessert': ['dessert', 'Ice-Cream', 'Cake', 'Sugar'],
+    'folding': ['folding', 'Folding'],
+    'knife': ['knife', 'Knife', 'Knives'],
+    'fork': ['fork', 'Fork', '"Tipsy Tavener"'],
+    'spoon': ['spoon', 'Ladle', 'ladle', 'Spoon'],
+    'stick': ['skewer', 'Skewer', 'Toothpick', 'nutpick','Chopsticks', 'Chopstick','chopstick', 'chopsticks'],
+    'utensil': ['Server', 'spreader', 'Spreader', 'Scoop', 'Sifter', 'Shears', 'corkscrew', 'Scraper', 'tongs', 'Tongs', 'Strainer', 'Peeler', 'peeler', 'pliers']
+}
+
+material_mapping = {
+    'animal': ['ivory', 'antler', 'bone', 'skin', 'sharkskin', 'hoof', 'leather', 'horn'],
+    'plant': ['wood', 'fiber'],
+    'metal': ['steel', 'silver', 'brass', 'gold', 'pewter', 'copper', 'metal'],
+    'shell': ['pearl', 'shell', 'tortoise'],
+    'other': ['lacquer', 'porcelain', 'stone', 'plastic', 'enamel', 'glass'],
+}
+
+# Function to map type
+def map_type(title, type_mapping):
+    title_lower = title.lower()  # Normalize the title to lowercase
+    matched_types = []
+    matched_subtypes = []
+
+    for type_category, keywords in type_mapping.items():
+        for keyword in keywords:
+            if keyword.lower() in title_lower:  # Check if the keyword exists in the title
+                matched_types.append(type_category)
+                matched_subtypes.append(keyword.lower())
+                break  # Stop after finding a match for the current category
+
+    if matched_types:
+        # Return the first matched type and the associated subtype
+        return matched_types[0], matched_subtypes[0] if matched_subtypes else None
+    return None, None
+
+
+
+# Function to map material
+def map_material(materials, mapping):
+    mapped = [mapping.get(material.split(' ')[0].lower(), material) for material in materials]
+    return mapped[0] if mapped else None, mapped[1] if len(mapped) > 1 else None
+
+
+# Helper function to process dates into a single year
+def process_date(date_content):
+    date_content = date_content.lower().strip()  # Normalize the string
+
+    # Match a single year (e.g., "1926")
+    if re.match(r"^\d{4}$", date_content):
+        return int(date_content)
+
+    # Match a year range (e.g., "1595–1700") and take the first year
+    if re.match(r"^\d{4}–\d{4}$", date_content):
+        start_year = int(date_content.split("–")[0])
+        return start_year
+
+    # Match approximate ranges (e.g., "ca. 1595–1700")
+    if date_content.startswith(("ca ", "ca.", "c. ")):
+        cleaned_content = re.sub(r"(ca\. |ca |c\. )", "", date_content).strip()  # Remove approximation
+        if "–" in cleaned_content:  # Handle range in approximated dates
+            start_year = int(cleaned_content.split("–")[0])
+            return start_year
+        if cleaned_content.isdigit():  # Handle single approximated year
+            return int(cleaned_content)
+
+    # Match approximate single year (e.g., "ca 1893", "c. 1893")
+    if date_content.startswith(("ca ", "c. ")):
+        approx_year = re.sub(r"(ca |c\. )", "", date_content).strip()
+        if approx_year.isdigit():
+            return int(approx_year)
+
+    # Match centuries (e.g., "19th century")
+    century_match = re.match(r"^(\d{1,2})(st|nd|rd|th) century$", date_content)
+    if century_match:
+        century = int(century_match.group(1))
+        year = (century - 1) * 100
+        return year
+
+    # Match decades (e.g., "2010s")
+    decade_match = re.match(r"^(\d{4})s$", date_content)
+    if decade_match:
+        decade_start_year = int(decade_match.group(1))
+        return decade_start_year
+
+    # Default fallback for unrecognized formats
+    return None
+
+
+
+
+
+
+
+
+
+
+
+# Process the JSON file
+# Updated processing logic for dates
+def process_json(input_path, output_path):
+    with open(input_path, 'r') as infile:
+        raw_data = json.load(infile)
+    
+    cleaned_data = []
+    for entry in raw_data:
+        ID = entry["id"]
+        title = entry["title"]
+        
+        # Map type and subtype
+        type_, subtype = map_type(title, type_mapping)
+
+        
+
+
+        # Extract and classify materials
+        materials = []
+        for desc in entry.get("description", []):
+            if desc.get("label", "").lower() in ["physical description", "medium"]:
+                # Split materials by commas or "and"
+                raw_materials = re.split(r",\s*| and ", desc["content"])
+                # Remove annotations in parentheses and clean up whitespace
+                materials.extend([re.sub(r"\s*\(.*?\)", "", mat).strip().lower() for mat in raw_materials])
+
+        # Classify materials using the material_mapping
+        classified_materials = []
+        for mat in materials:
+            found_category = None
+            for category, keywords in material_mapping.items():
+                if mat in keywords:
+                    found_category = category
+                    break
+            classified_materials.append(found_category if found_category else "unknown")
+
+        # Assign the first classified material as the primary material
+        material = classified_materials[0] if classified_materials else "unknown"
+        submaterial = ", ".join(materials) if materials else ""
+
+
+        
+        # Process dates
+        date_data = entry.get("date", [])
+        year = None
+        for date in date_data:
+            if date.get("label", "").lower() in ["date made", "date"]:  # Check for multiple labels
+                year = process_date(date.get("content", ""))
+                if year:
+                    break
+
+        
+        # Extract country
+        # Extract country
+        places = [
+            place.get("content", "") 
+            for place in entry.get("place", []) 
+            if place.get("label", "").lower() in ["place made", "place", "made in"]
+        ]
+        country = places[-1].split(": ")[-1].strip() if places else None
+
+        
+        # Append cleaned entry
+        cleaned_data.append({
+            "ID": ID,
+            "type": type_,
+            "subtype": subtype,
+            "material": material,
+            "submaterial": submaterial,
+            "year": year,
+            "country": country
+        })
+    
+    # Write the cleaned data to a new JSON file
+    with open(output_path, 'w') as outfile:
+        json.dump(cleaned_data, outfile, indent=4)
+
+
+# Paths to the JSON files
+input_json_path = 'cutleryOrig.json'  # Update this with the path to your input file
+output_json_path = 'cleaned_data.json'  # Update this with the desired output file path
+
+# Process the data
+process_json(input_json_path, output_json_path)
+
+print(f"Data has been cleaned and saved to {output_json_path}.")
